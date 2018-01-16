21192e57d22c7c88b8def6d7b1b90ee1f52321dc
Jason Rushton <jasonr@busybusy.com>
2018-01-16 16:05:42 -0700

    expand Mod schema
    add schema type Tag
    add schema type MediaFile
    add schema type MediaFileUrls
    add schema type Release
    add schema type ReleaseInfo

--- index.js
+++ index.js
@@ -34,23 +34,73 @@ const typeDefs = `
   }
   
   type Mod { 
-      id: Int, 
-      title: String, 
-      name: String, 
-      owner: String,
-      summary: String,
+      id: Int 
+      title: String 
+      name: String 
+      owner: String
+      summary: String
       
-      game_versions: [String], 
-      downloads_count: Int, 
-      current_user_rating: Int, 
-      ratings_count: Int, 
-      github_path: String,
-      updated_at: String,
-      license_flags: Int, 
-      license_name: String, 
-      license_url: String, 
-      homepage: String, 
-      created_at: String, 
+      game_versions: [String] 
+      downloads_count: Int 
+      current_user_rating: Int 
+      ratings_count: Int 
+      github_path: String
+      updated_at: String
+      license_flags: Int 
+      license_name: String 
+      license_url: String 
+      homepage: String 
+      created_at: String 
+	  
+	  tags: [Tag],
+	  first_media_file: MediaFile
+	  latest_release: Release
+  }
+  
+  type Tag {
+	  id: Int
+	  name: String
+	  title: String
+	  description: String
+	  type: String 	  
+  }
+  
+  type MediaFile {
+	  id: Int
+	  height: Int
+	  width: Int
+	  size: Int
+	  urls: MediaFileUrls
+  }
+  
+  type MediaFileUrls {
+	  original: String
+	  thumb: String
+  }
+  
+  type Release {
+	  id: Int
+	  download_url: String
+	  downloads_count: Int
+	  factorio_version: String
+	  file_name: String
+	  file_size: Int
+	  game_version: String
+	  released_at: String
+	  version: String
+  }
+  
+  
+  type ReleaseInfo {
+  	author: String
+  	contact: String
+  	description: String
+  	dependencies: [String]
+  	factorio_version: String
+  	homepage: String
+  	name: String
+  	title: String
+  	version: String
   }
 `;
 
